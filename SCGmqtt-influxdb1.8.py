4766064c5d57bc4ea8812f99edeec485afa6d263
root <nguyendat5307@gmail.com>
2020-07-30 23:05:44 +0700

    modified:   SCGmqtt-influxdb1.8.py

--- SCGmqtt-influxdb1.8.py
+++ SCGmqtt-influxdb1.8.py
@@ -4,7 +4,7 @@ import datetime
 import pandas as pd
 from influxdb import DataFrameClient
 
-dbclient = DataFrameClient(host='127.0.0.1', port=8086, username='root', password='root', database='SCG')
+dbclient = DataFrameClient(host='nangluong.iotmind.vn', port=8086, username='root', password='root', database='SCG')
 dbclient.create_database('SCG')
 
 def on_connect(client, userdata, flags, rc):
@@ -19,14 +19,6 @@ def on_message(client, userdata, msg):
     data[['device', 'parameter']] = data.tag.str.split(':', expand=True)
     data['topic'] = topic
     data['time'] = datetime.datetime.utcnow()
-    local = datetime.datetime.now()
-    if (local.hour >=7) and (local<15):
-        ca = "ca1"
-    elif (local.hour >=15) and (local<23):
-        ca = "ca2"
-    else:
-        ca = "ca3"
-    data["ca"] = ca
     data.set_index('time', inplace=True)
     data = data.drop(['tag'], axis=1)
     dbclient.write_points(data, 'realtime', field_columns=['value'])
@@ -35,6 +27,6 @@ client = mqtt.Client()
 client.username_pw_set(username="mind", password="123")
 client.on_connect = on_connect
 client.on_message = on_message
-
+# Connect
 client.connect("nangluong.iotmind.vn", 16766, 60)
 client.loop_forever()
