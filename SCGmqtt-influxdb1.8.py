1f9f077963c071a5398f78dc42a9001a69a34f74
Đức Đạt Nguyễn <nguyenducdat.dut@gmail.com>
2020-07-31 12:20:45 +0700

    mod SCGmqtt

--- SCGmqtt-influxdb1.8.py
+++ SCGmqtt-influxdb1.8.py
@@ -17,6 +17,9 @@ def on_message(client, userdata, msg):
     data = objpayload["d"]
     data = pd.DataFrame(data)
     data[['device', 'parameter']] = data.tag.str.split(':', expand=True)
+    data[['parameter_trans', 'feeder_trans']] = data.parameter.str.split('_', expand=True)
+    data.replace({'parameter_trans':{'klt':'Sum. Volume', 'tld':'Set point rate', 'tlph':'Actual rate', 'nsd':'Set point capacity', 'nsph':'Actual capacity'}}, inplace=True)
+    data.replace({'feeder_trans':{'c1':'Clinker 1', 'c2':'Clinker 2', 'c3':'Clinker 3', 'pg2':'Additives 2', 'pgm':'New additives', 'thachcao':'Plaster', 'pg3':'Additives 3', 'trobay':'Fly ash'}}, inplace=True)
     data['topic'] = topic
     data['time'] = datetime.datetime.utcnow()
     data.set_index('time', inplace=True)
